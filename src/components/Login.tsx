8ab8cfb51d843715ec4940b5cb08a9f12083efe3
Gonzalo <zavalagonzalo172@gmail.com>
2024-08-21 10:38:50 -0300

    Cosas hechas en la facu

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,8 @@
+import Ojo from "./icons/Ojo";
+
 export default function Login() {
   return (
-    <main className="h-screen w-[80vw] rounded-[10rem] bg-slate-200 p-10 md:h-[60vh]">
+    <main className="h-screen w-[80vw] rounded-[10rem] bg-slate-200 p-10 text-black md:h-[60vh]">
       <div className="flex flex-col md:flex-row">
         <div className="m-auto">
           <img src="/girando.gif" alt="Capy!" />
@@ -24,8 +26,29 @@ export default function Login() {
             </div>
 
             <div className="flex flex-col">
-              <label>Password</label>
-              <input type="password" placeholder="superSecurePassword" />
+              <label className="block">Password</label>
+              <div className="relative">
+                <input
+                  id="password"
+                  type="password"
+                  className="block w-full rounded-lg border-gray-200 py-3 pe-10 ps-4 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50"
+                  placeholder="Enter password"
+                />
+                <button
+                  type="button"
+                  onClick={() => {
+                    const field = document.getElementById(
+                      "password",
+                    ) as HTMLInputElement;
+
+                    if (field.type === "password") field.type = "text";
+                    else field.type = "password";
+                  }}
+                  className="text-primary absolute inset-y-0 end-0 z-20 flex cursor-pointer items-center rounded-e-md px-3 focus:text-blue-600 focus:outline-none dark:focus:text-blue-500"
+                >
+                  <Ojo />
+                </button>
+              </div>
             </div>
 
             <button
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgba(255, 255, 255, 0.87)"
+      }
+    },
   },
   plugins: [],
 }
