508ed06955dd0887ea7e3d39fc9eb9f8d343fac8
Brenda Tapari <99502@frc.utn.edu.ar>
2024-09-05 18:43:38 -0300

    Agus agregue el coso!

--- src/components/Inicio.tsx
+++ src/components/Inicio.tsx
@@ -1,14 +1,14 @@
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
-import { Button } from './ui/button'
 import { Input } from "@/components/ui/input"
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger
 } from '@/components/ui/tooltip'
+import { Button } from './ui/button'
 
-import { Hourglass, Timer, Trash, Edit3, Star } from 'lucide-react'
+import { Edit3, Hourglass, Star, Timer, Trash } from 'lucide-react'
 
 import {
   Select,
@@ -19,6 +19,48 @@ import {
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
+
+import { Check, ChevronsUpDown } from "lucide-react"
+import * as React from "react"
+ 
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+ 
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+]
+ 
 
 
 import { useState } from 'react'
@@ -33,6 +75,9 @@ const descriptions: Record<CapyMetodos, string> = {
 }
 
 export default function Inicio() {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
   const [description, setDescription] =
     useState<keyof typeof descriptions>('Capydoro')
 
@@ -47,8 +92,10 @@ export default function Inicio() {
     }
 
   const [objetivos, setObjetivos] = useState<string[]>([])
-  const [objetivosFav, setObjetivosFav] = useState<string[]>([]) // vacia
-  const [value, setValue] = useState('')
+  const [objetivosFav, setObjetivosFav] = useState<string[]>([]) 
+  
+  // vacia
+  /* const [value, setValue] = useState('') */
   const [index, setIndex] = useState<number|null>(null) // hasta que se toque
 
   const handleAdd = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -203,6 +250,49 @@ export default function Inicio() {
           </div>
         </div>
       </section>
+      <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : "Select framework..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {framework.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
     </>
   )
 }
