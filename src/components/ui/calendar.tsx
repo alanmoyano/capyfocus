f6347a13e2e288542b4b28b55f22644bbb1ebe96
Alan Moyano <alanmoyano203@gmail.com>
2024-09-06 19:40:20 -0300

    Anda!

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,10 +52,8 @@ function Calendar({
         ...classNames
       }}
       components={{
-        // eslint-disable-next-line
-        IconLeft: ({ ...props }) => <ChevronLeft className='h-4 w-4' />,
-        // eslint-disable-next-line
-        IconRight: ({ ...props }) => <ChevronRight className='h-4 w-4' />
+        IconLeft: () => <ChevronLeft className='h-4 w-4' />,
+        IconRight: () => <ChevronRight className='h-4 w-4' />
       }}
       {...props}
     />
