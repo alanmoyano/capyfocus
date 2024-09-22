04695ee86a3f7f039247ff046e07e2568af7d3bc
Maria Agustina Nievas Britos <89545@sistemas.frc.utn.edu.ar>
2024-09-22 12:20:25 -0300

    capyDatos

--- src/components/Usuario.tsx
+++ src/components/Usuario.tsx
@@ -18,6 +18,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
@@ -44,25 +50,80 @@ export default function Usuario() {
                 <img src='/idle.gif' />
             </div>
         </Card>
-        <Card className='h-full w-full'>
+        <Card className='h-full w-full shadow-md bg-secondary'>
           <CardHeader className='text-center'>
-            <CardTitle className='text-xl'>Hola, @capyUsuario!</CardTitle>
+            <CardTitle className='text-xl py-4'>Hola, @capyUsuario!</CardTitle>
             <CardDescription className='text-center'>
-            <Avatar className='mx-auto w-32 h-32'>
-              <AvatarImage src='/capyPic.png' className='w-full h-full' />
-              <AvatarFallback className='text-2xl'>CN</AvatarFallback>
-            </Avatar>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <Avatar className='mx-auto w-32 h-32'>
+                          <AvatarImage src='/capyPic.png' className='w-full h-full' />
+                            <AvatarFallback className='text-2xl'>CN</AvatarFallback>
+                        </Avatar>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>Datos del perfil</DialogTitle>
+                          <DialogDescription>
+                            Make changes to your profile here. Click save when you're done.
+                          </DialogDescription>
+                        </DialogHeader>
+                          <div className="grid gap-4 py-4">
+                            <div className="grid grid-cols-4 items-center gap-4">
+                              <Label htmlFor="username" className="text-right">
+                                Username
+                              </Label>
+                              <Input
+                                id="username"
+                                defaultValue="Chicho Perez"
+                                className="col-span-3"
+                              />
+                            </div>
+                            <div className="grid grid-cols-4 items-center gap-4">
+                              <Label htmlFor="email" className="text-right">
+                                Email
+                              </Label>
+                              <Input
+                                id="email"
+                                defaultValue="hola@chicho.com"
+                                className="col-span-3"
+                              />
+                            </div>
+                          </div>
+                          <DialogFooter>
+                            <Button type="submit">Confirmar</Button>
+                          </DialogFooter>
+                        </DialogContent>
+                      </Dialog>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Modificar datos del perfil</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </CardDescription>
           </CardHeader>
           <CardContent className='text-center'>
-            <p className='text-lg'>Card Content</p>
+            <p className='text-xl'>Chicho Perez</p>
+            <div className='m-4'>
+              <div className="grid w-full max-w-sm items-start gap-1.5 text-left p-4">
+                <Label htmlFor="email">Email</Label>
+                <Input disabled type="mail" id="email" placeholder="hola@chicho.com" />
+              </div>
+              <div className="grid w-full max-w-sm items-start gap-1.5 text-left p-4">
+                <Label htmlFor="username">Usuario</Label>
+                <Input disabled type="username" id="username" placeholder="Chichito24" />
+              </div>
+            </div>
           </CardContent>
-          <CardFooter className='text-center'>
-            <p>Card Footer</p>
-          </CardFooter>
-          <Button onClick={() => handleLogin()} className='mt-4'>
+          <CardFooter className='flex justify-end'>
+            <Button onClick={() => handleLogin()} className='mt-4'>
               Inciar sesion
             </Button>
+          </CardFooter>
         </Card>
       </div>
      </div>
