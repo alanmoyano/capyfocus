6582ba6b7335958a18bebcc63cbcb9488b289733
Alan Moyano <alanmoyano203@gmail.com>
2024-09-30 13:21:46 -0300

    testing env

--- src/components/supabase/client.ts
+++ src/components/supabase/client.ts
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY
 )
