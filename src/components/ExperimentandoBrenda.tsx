2c4a0e3d9f7d46b04a19f5e296d9f3b7b97411e0
Brenda Tapari <99502@frc.utn.edu.ar>
2024-10-19 19:42:58 -0300

    Landing

--- src/components/ExperimentandoBrenda.tsx
+++ src/components/ExperimentandoBrenda.tsx
@@ -1,129 +1,177 @@
+import { Button } from '@/components/ui/button'
+import { Timer, Hourglass } from 'lucide-react'
 import { useLocation } from 'wouter'
 
 export default function ExperimentandoBrenda() {
   const [, setLocation] = useLocation()
 
-  function handleNavigate() {
+  function handleNavigateInicio() {
     setLocation('/')
+  }
+
+  function handleNavigateRegistrar() {
+    setLocation('/registrate')
   }
 
   return (
     <div>
       <div className='bg-white'>
-        <header className='flex items-center justify-end p-6'>
-          <nav className='space-x-4'>
-            <a href='/login' className='text-lg text-gray-600'>
-              Iniciar sesión
-            </a>
-            <a
-              href='/login'
-              className='rounded-lg bg-primary px-4 py-2 text-white'
-            >
-              Regístrate
-            </a>
-          </nav>
-        </header>
-
         {/* Comienzo */}
-        <section className='my-10 text-center'>
-          <h1 className='mb-4 text-4xl font-bold'>
-            Optimiza tu tiempo de estudio con CapyFocus
+        <section className='my-10 bg-gradient-to-r from-blue-50 to-purple-50 p-10 text-center'>
+          <h1 className='mb-4 text-5xl font-extrabold text-gray-800'>
+            Optimiza tu tiempo de estudio con{' '}
+            <span className='text-[#76aab4]'>CapyFocus</span>
           </h1>
-          <p className='mb-6 text-xl'>
+          <p className='mb-6 text-2xl text-gray-700'>
             Capydoros y Capymetro, dos técnicas que te ayudarán a lograr tus
             objetivos.
           </p>
-          <a
-            href='/'
-            className='rounded-lg bg-primary px-8 py-4 text-lg text-white'
-          >
-            Empieza ahora
-          </a>
+          <div className='flex items-center justify-around py-12'>
+            <div className='max-w-md transform transition-transform duration-300 hover:scale-105'>
+              <h2 className='mb-2 text-3xl font-bold text-[#76aab4]'>
+                <span className='flex items-center justify-center gap-4'>
+                  <Timer />
+                  Capydoro
+                </span>
+              </h2>
+              <p className='text-lg text-gray-600'>
+                Basado en la técnica pomodoro, para sesiones de estudio
+                eficientes.
+              </p>
+            </div>
+            <div className='max-w-md transform transition-transform duration-300 hover:scale-105'>
+              <h2 className='mb-2 text-3xl font-bold text-[#76aab4]'>
+                <span className='flex items-center justify-center gap-4'>
+                  <Hourglass />
+                  Capymetro
+                </span>
+              </h2>
+              <p className='text-lg text-gray-600'>
+                Un cronómetro para medir el tiempo de estudio sin
+                interrupciones.
+              </p>
+            </div>
+          </div>
         </section>
-
-        <section className='my-16 flex justify-around'>
-          <div className='max-w-md'>
-            <h2 className='mb-2 text-2xl font-semibold'>Capydoros</h2>
-            <p className='text-lg'>
-              Basado en la técnica pomodoro, para sesiones de estudio
-              eficientes.
-            </p>
-          </div>
-          <div className='max-w-md'>
-            <h2 className='mb-2 text-2xl font-semibold'>Capymetro</h2>
-            <p className='text-lg'>
-              Un cronómetro para medir el tiempo de estudio sin interrupciones.
-            </p>
-          </div>
+ {/* Botones */}
+        <section className='py-8'>
+          <p className='text-center text-2xl font-semibold text-gray-800'>
+            ¡El momento es ahora! ¿Listo para tomar el control de tu tiempo de
+            estudio?
+          </p>
+          <p className='mt-4 text-center text-lg text-gray-700'>
+            Con cada sesión, te acercas más al éxito. No dejes que el tiempo se
+            te escape, ¡haz que cada minuto cuente!
+          </p>
+          <p className='mt-6 text-center text-lg text-gray-700'>
+            ¿Qué estás esperando? Únete a nosotros y empieza a disfrutar de un
+            estudio más productivo y efectivo.
+          </p>
+          <span className='mt-12 flex w-full justify-center gap-10'>
+            <Button
+              className='rounded-lg border border-gray-300 bg-white p-8 text-xl text-gray-900 shadow-md transition-all duration-300 hover:bg-gray-100 hover:shadow-lg'
+              onClick={handleNavigateRegistrar}
+            >
+              Registrate
+            </Button>
+            <Button
+              className='rounded-lg bg-[#76aab4] p-8 text-xl text-white shadow-md transition-all duration-300 hover:shadow-lg'
+              onClick={handleNavigateInicio}
+              variant={'accent'}
+            >
+              Empieza ahora
+            </Button>
+          </span>
         </section>
 
         {/* Eventos y Estadisticas */}
 
-        <section className='bg-gray-100 py-10 text-center'>
-          <h2 className='mb-4 text-3xl font-bold'>
+        <section className='bg-gradient-to-r from-blue-50 to-purple-50 py-10 text-center'>
+          <h2 className='mb-4 text-4xl font-bold text-gray-700'>
             Crea eventos y alcanza tus metas
           </h2>
-          <p className='mb-6 text-lg'>
-            ¿Tienes un examen o un proyecto importante? Con CapyFocus puedes
-            crear eventos y asignarles objetivos específicos para estar siempre
-            en control de tu progreso.
+          <span className='mb-2 text-lg font-semibold text-gray-600'>
+            ¿Tienes un examen o un proyecto importante?
+          </span>
+          <p className='mb-6 text-lg text-gray-600'>
+            Con CapyFocus puedes crear eventos y asignarles objetivos
+            específicos para estar siempre en control de tu progreso.
           </p>
           <div className='flex justify-center space-x-6'>
-            <div className='max-w-sm transform rounded-lg bg-white p-6 shadow-lg transition duration-300 hover:scale-105'>
+            <div className='max-w-sm transform rounded-lg bg-accent/60 p-6 shadow-xl transition duration-300 hover:scale-105'>
               <img
-                src='public/FotosLanding/eventos.png'
+                src='/FotosLanding/eventos2.png'
                 alt='Eventos'
-                className='mx-auto mb-6 h-40 w-full object-cover'
+                className='mx-auto mb-6 h-40 w-full rounded-lg object-cover'
               />
-              <h3 className='mb-4 text-2xl font-semibold'>
+              <h3 className='mb-4 text-2xl font-semibold text-gray-700'>
                 Organiza tus exámenes
               </h3>
-              <p className='text-lg'>
+              <p className='text-lg text-gray-600'>
                 Define eventos importantes como exámenes o entregas y crea
                 objetivos personalizados.
               </p>
             </div>
-            <div className='max-w-sm transform rounded-lg bg-white p-6 shadow-lg transition duration-300 hover:scale-105'>
+            <div className='max-w-sm transform rounded-lg bg-accent/60 p-6 shadow-xl transition duration-300 hover:scale-105'>
               <img
-                src='public/FotosLanding/estadisticas.png'
+                src='/FotosLanding/estadisticas.png'
                 alt='Objetivos'
-                className='mx-auto mb-6 h-40 w-full object-cover'
+                className='mx-auto mb-6 h-40 w-full rounded-lg object-cover'
               />
-              <h3 className='mb-4 text-2xl font-semibold'>Sigue tu progreso</h3>
-              <p className='text-lg'>
+              <h3 className='mb-4 text-2xl font-semibold text-gray-700'>
+                Sigue tu progreso
+              </h3>
+              <p className='text-lg text-gray-600'>
                 Asocia objetivos a cada evento y sigue tu evolución en tiempo
                 real.
               </p>
             </div>
-            <div className='max-w-sm transform rounded-lg bg-white p-6 shadow-lg transition duration-300 hover:scale-105'>
+            <div className='max-w-sm transform rounded-lg bg-accent/60 p-6 shadow-xl transition duration-300 hover:scale-105'>
               <img
-                src='public\FotosLanding\CapyInsignias.png'
+                src='\FotosLanding\CapyInsignias.png'
                 alt='Insignia 1'
-                className='mx-auto mb-6 h-40 w-full object-cover'
+                className='mx-auto mb-6 h-40 w-full rounded-lg object-cover'
               />
-              <h3 className='mb-4 text-2xl font-semibold'>
+              <h3 className='mb-4 text-2xl font-semibold text-gray-700'>
                 Desbloquea CapyInsignias
               </h3>
-              <p className='text-lg'>
+              <p className='text-lg text-gray-600'>
                 A medida que avances en tus estudios, irás desbloqueando
                 insignias que representan tu progreso.
               </p>
             </div>
           </div>
         </section>
+{/* Chicho! */}
+        <section className='my-16  flex items-center justify-center'>
+          {/* Columna 1 */}
+          <div className='w-2/3 text-left ml-16'>
+            <p className='text-2xl font-semibold text-gray-800'>
+              ¡Chicho te ayudara a mantenerte enfocado!
+            </p>
+            <p className='mt-4 text-lg text-gray-700'>
+              Dependiendo de tu estilo de motivación, Chicho puede ser dulce y
+              alentador, o firme y directo para que no pierdas el enfoque.
+              ¡Elige cómo te hablará y aprovecha cada sesión al máximo!
+            </p>
+            <p className='mt-4 text-lg text-gray-700'>
+              Ajusta su actitud en tu perfil y disfruta de una experiencia de
+              estudio totalmente personalizada con CapyFocus.
+            </p>
+          </div>
 
-        <section className='my-16 text-center'>
-          <img
-            src='public\Chicho\OtrasAcciones\CapyOk.gif'
-            alt='Chicho'
-            className='mx-auto w-32'
-          />
-          <div className='mt-4'>
-            <p className='text-lg'>¡Chicho te ayudará a mantenerte enfocado!</p>
+          {/* Columna 2 */}
+          <div className='w-1/3 flex text-center'>
+            <img
+              src='\Chicho\OtrasAcciones\CapyOk.gif'
+              alt='Chicho'
+              className='mx-auto w-60'
+            />
           </div>
         </section>
-
-        <section className='bg-gray-50 py-12'>
+  
+          {/* Sobre Nosotros */}
+        <section className='bg-gradient-to-r from-blue-50 to-purple-50 py-12'>
           <div className='mx-auto max-w-7xl text-center'>
             <h2 className='mb-6 text-3xl font-bold'>Sobre Nosotros</h2>
             <p className='mx-auto mb-8 max-w-2xl text-lg'>
@@ -134,19 +182,19 @@ export default function ExperimentandoBrenda() {
 
             <div className='mb-8 flex justify-center space-x-6'>
               <img
-                src=''
+                src='./FotosLanding/presentandoCapyfocus2.jpg'
                 alt='Foto del equipo 1'
-                className='h-32 w-32 rounded-full object-cover shadow-lg'
+                className='h-44 w-44 rounded-full object-cover shadow-lg'
               />
               <img
-                src=''
+                src='./FotosLanding/amigos.png'
                 alt='Foto del equipo 2'
-                className='h-32 w-32 rounded-full object-cover shadow-lg'
+                className='h-44 w-44 rounded-full object-cover shadow-lg'
               />
               <img
-                src=''
+                src='./FotosLanding/Chicho.png'
                 alt='Foto del equipo 3'
-                className='h-32 w-32 rounded-full object-cover shadow-lg'
+                className='h-44 w-44 rounded-full object-cover shadow-lg'
               />
             </div>
 
