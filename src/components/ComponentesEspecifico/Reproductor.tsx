0267a54e0686ebf6ce13520e17ed36511279447d
Alan Moyano <alanmoyano203@gmail.com>
2024-11-09 20:45:40 -0300

    Prueba (espero) final

--- src/components/ComponentesEspecifico/Reproductor.tsx
+++ src/components/ComponentesEspecifico/Reproductor.tsx
@@ -10,16 +10,25 @@ export default function Reproductor({
   src: `${string}.webm`
   className?: string
 }) {
+  const test = true
+
   return (
-    <video
-      autoPlay
-      playsInline
-      muted
-      loop
-      className={cn('aspect-auto', className)}
-    >
-      <source src={src.split('.')[0].concat('.mov')} />
-      <source src={src} />
-    </video>
+    <>
+      {test ? (
+        <img src={src.split('.')[0].concat('.avif')} />
+      ) : (
+        <video
+          autoPlay
+          playsInline
+          muted
+          loop
+          className={cn('aspect-auto', className)}
+        >
+          <source src={src.split('.')[0].concat('.mp4')} />
+          <source src={src.split('.')[0].concat('.mov')} />
+          <source src={src} />
+        </video>
+      )}
+    </>
   )
 }
