09846059949fc3efdafbd03405102b0769fd822e
Alan Moyano <alanmoyano203@gmail.com>
2025-04-14 18:28:21 -0300

    Update .gitignore, remove auth-schema, enhance next.config and page components, and add .env.example

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "88zflda82j.ufs.sh",
+        port: "",
+        pathname: "/f/**",
+      },
+    ],
+  },
   experimental: {
     ppr: true,
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useSession } from "@/lib/auth-client"
+import Image from "next/image"
 
 export default function Home() {
   const { data: session } = useSession()
@@ -8,6 +9,14 @@ export default function Home() {
   return (
     <div>
       <p>Bienvenido {session ? session.user.name : "invitado"}</p>
+      <Image
+        src="https://88zflda82j.ufs.sh/f/dz6jW9yscKwL1COaPhHc4GQPuROEC5ZhHX0NYMBFiTortvkD"
+        alt="Chicho en un auto"
+        width={480}
+        height={480}
+        unoptimized
+      />
+      <p>texto que no se va a mover, gracias next/image</p>
     </div>
   )
 }
